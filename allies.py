9ba08717b807015db70d3d99840c9fb0b1e6fddd
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2024-08-28 16:17:33 +0100

    Allies of Skin

---
+++ allies.py
@@ -0,0 +1,7 @@
+from company_interface import create_interface
+
+
+if __name__ == "__main__":
+    company_name = "Allies of Skin"
+    emoji = "🧴"
+    create_interface(company_name, emoji, "allies")
