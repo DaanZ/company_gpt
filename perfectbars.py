e32cb8d256e934fc5dc553ad09c4dfb341ab35dd
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2024-08-30 12:54:45 +0100

    Perfect Bar

---
+++ perfectbars.py
@@ -0,0 +1,7 @@
+from company_interface import create_interface
+
+
+if __name__ == "__main__":
+    company_name = "Perfect Bar"
+    emoji = "🥜🍪🍯"
+    create_interface(company_name, emoji, "perfectbars")
