fe9bc39b4a2a54688aa8b5631e9fc8ab821436fa
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2024-09-04 12:16:37 +0200

    psych

--- psycho_bunny.py
+++ psycho_bunny.py
@@ -4,4 +4,4 @@ from company_interface import create_interface
 if __name__ == "__main__":
     company_name = "Psycho Bunny"
     emoji = "💀🐰"
-    create_interface(company_name, emoji, "psycho")
+    create_interface(company_name, emoji, "psycho_bunny")
