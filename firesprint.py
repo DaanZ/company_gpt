185e5464d546655605c4090592465e43f6ad5c27
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2024-10-03 15:25:45 +0700

    Reports

---
+++ firesprint.py
@@ -0,0 +1,19 @@
+
+from history import History
+from loader import load_knowledge_logs
+from streaming_interface import streaming_interface
+
+
+if __name__ == "__main__":
+    company_name = "FireSprint"
+    emoji = "🖨️🎨📊"
+    company_id = "firesprint"
+    # Main program logic (call this function when you want to start the thread)
+    try:
+        #start_transcript_thread()  # Start the thread, will only start once
+        history: History = load_knowledge_logs(company_name, f"data/{company_id}.json")
+        print(len(history.logs))
+        streaming_interface(company_name, emoji, history)
+    except KeyboardInterrupt:
+        print("Program interrupted.")
+
