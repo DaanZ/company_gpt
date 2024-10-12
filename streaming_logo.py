da1dcf1744ca869f73610c49cd06199638129d70
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2024-10-12 11:00:25 +0700

    UI changes and Question integration

--- streaming_logo.py
+++ streaming_logo.py
@@ -42,53 +42,57 @@ def streaming_logo_interface(company_name: str, emoji: str, history: History, op
         }}
         </style>
         """, unsafe_allow_html=True)
-    # Initialize history if not already in session state
-    if "history" not in st.session_state.keys():
-        st.session_state.history = history
-        if prompt is None:
-            st.session_state.history.system(
-                f"""You are a very kindly and friendly marketing assistant for {company_name}. 
-            You are currently having a conversation with a marketing person. Answer the questions in a kind and friendly 
-            way, being the expert for {company_name} to answer any questions about marketing.""")
-        else:
-            st.session_state.history.system(prompt)
 
-        if optional:
-            st.session_state.history.system(optional)
-            with st.chat_message("system"):
-                st.markdown(optional)
+    col1, col2 = st.columns([7,3])
+    with col1:
+        # Initialize history if not already in session state
+        if "history" not in st.session_state.keys():
+            st.session_state.history = history
+            if prompt is None:
+                st.session_state.history.system(
+                    f"""You are a very kindly and friendly marketing assistant for {company_name}. 
+                You are currently having a conversation with a marketing person. Answer the questions in a kind and friendly 
+                way, being the expert for {company_name} to answer any questions about marketing.""")
+            else:
+                st.session_state.history.system(prompt)
 
-    # Display all previous messages
-    for message in st.session_state.history.logs:
-        if message["role"] == "system":
-            continue
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            if optional:
+                st.session_state.history.system(optional)
+                with st.chat_message("system"):
+                    st.markdown(optional)
+
+        # Display all previous messages
+        for message in st.session_state.history.logs:
+            if message["role"] == "system":
+                continue
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
     user_prompt = st.chat_input()  # Input box for the user
 
-    if user_prompt is not None:
-        st.session_state.history.user(user_prompt)
-        with st.chat_message("user"):
-            st.markdown(user_prompt)
+    with col1:
+        if user_prompt is not None:
+            st.session_state.history.user(user_prompt)
+            with st.chat_message("user"):
+                st.markdown(user_prompt)
 
-        # Placeholder for the assistant's reply
-        assistant_message_placeholder = st.chat_message("assistant")
-        assistant_text = assistant_message_placeholder.empty()
+            # Placeholder for the assistant's reply
+            assistant_message_placeholder = st.chat_message("assistant")
+            assistant_text = assistant_message_placeholder.empty()
 
-        # Stream response
-        with st.spinner("Loading..."):
-            if pages:
-                print("langchain")
-                db = FAISS.from_documents(pages, OpenAIEmbeddings())
-                for response in db.similarity_search(user_prompt, k=3):
-                    print("article: " + response.page_content)
-                    st.session_state.history.system(response.page_content)
-                response_stream = llm_stream(st.session_state.history)
-            else:
-                response_stream = llm_stream(st.session_state.history)
-            answers = process_stream(response_stream)
-            chunk = ""
-            for chunk in answers:
-                assistant_text.markdown(chunk)  # Update progressively
-            st.session_state.history.assistant(chunk)  # Save final message in history
+            # Stream response
+            with st.spinner("Loading..."):
+                if pages:
+                    print("langchain")
+                    db = FAISS.from_documents(pages, OpenAIEmbeddings())
+                    for response in db.similarity_search(user_prompt, k=3):
+                        print("article: " + response.page_content)
+                        st.session_state.history.system(response.page_content)
+                    response_stream = llm_stream(st.session_state.history)
+                else:
+                    response_stream = llm_stream(st.session_state.history)
+                answers = process_stream(response_stream)
+                chunk = ""
+                for chunk in answers:
+                        assistant_text.markdown(chunk)  # Update progressively
+                st.session_state.history.assistant(chunk)  # Save final message in history
