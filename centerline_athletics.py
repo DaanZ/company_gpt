751e1fb83fa0c27f53bf4c9ace7f36ed134004e1
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2024-12-11 14:31:54 +0700

    centerline

---
+++ centerline_athletics.py
@@ -0,0 +1,19 @@
+
+from history import History
+from loader import load_knowledge_logs
+from streaming_interface import streaming_interface
+
+
+if __name__ == "__main__":
+    company_name = "Centerline Atheletics"
+    emoji = "🎾"
+    company_id = "centerline_athletics"
+
+    # Main program logic (call this function when you want to start the thread)
+    try:
+        history: History = load_knowledge_logs(company_name, f"data/{company_id}.json")
+        print(len(history.logs))
+        streaming_interface(company_name, emoji, history)
+    except KeyboardInterrupt:
+        print("Program interrupted.")
+
