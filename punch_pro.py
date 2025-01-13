5dc46940aba0ab160c84035074482143318a1b94
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-01-13 21:38:57 +0700

    punch pro

---
+++ punch_pro.py
@@ -0,0 +1,18 @@
+
+from history import History
+from loader import load_knowledge_logs
+from streaming_interface import streaming_interface
+
+
+if __name__ == "__main__":
+    company_name = "Punch Pro"
+    emoji = "⏰"
+    company_id = "punch_pro"
+
+    # Main program logic (call this function when you want to start the thread)
+    try:
+        history: History = load_knowledge_logs(company_name, f"data/{company_id}.json")
+        print(len(history.logs))
+        streaming_interface(company_name, emoji, history)
+    except KeyboardInterrupt:
+        print("Program interrupted.")
