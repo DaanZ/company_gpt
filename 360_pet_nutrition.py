9516a140b0756a2211de1c921786c72a58b95dba
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-10 18:10:44 +0700

    Pets v 2

---
+++ 360_pet_nutrition.py
@@ -0,0 +1,8 @@
+from faq import FAQ_interface
+
+
+if __name__ == "__main__":
+    company_name = "360 Pet Nutrition"
+    emoji = "🍖"
+    company_id = "360_pet_nutrition"
+    FAQ_interface(company_id, company_name, emoji)
