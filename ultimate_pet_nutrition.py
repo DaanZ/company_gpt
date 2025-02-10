08a959234072dda26e9d409cbbbd0dbaa7657816
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-10 18:04:50 +0700

    Update ultimate_pet_nutrition.py

--- ultimate_pet_nutrition.py
+++ ultimate_pet_nutrition.py
@@ -4,5 +4,5 @@ from faq import FAQ_interface
 if __name__ == "__main__":
     company_name = "Ultimate Pet Nutrition"
     emoji = "🍖"
-    company_id = "Ultimate Pet Nutrition"
+    company_id = "ultimate_pet_nutrition"
     FAQ_interface(company_id, company_name, emoji)
