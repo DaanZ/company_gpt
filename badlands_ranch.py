2af497212b62571491496ae622c500f8b5117a36
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-10 19:38:12 +0700

    Petfood 4

---
+++ badlands_ranch.py
@@ -0,0 +1,9 @@
+from faq import FAQ_interface
+
+
+if __name__ == "__main__":
+    company_name = "Badlands Ranch"
+    emoji = "🍖"
+    company_id = "badlands_ranch"
+    FAQ_interface(company_id, company_name, emoji)
+    
