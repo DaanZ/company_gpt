ed30659af4267efe65e4d1b57fb211f91279768d
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-11 23:30:13 +0700

    rwest

---
+++ rwest.py
@@ -0,0 +1,8 @@
+from faq import FAQ_interface
+
+
+if __name__ == "__main__":
+    company_name = "RWest"
+    emoji = "🚀"
+    company_id = "rwest"
+    FAQ_interface(company_id, company_name, emoji)
