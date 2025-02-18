14c7d723ae1acd05a39d836e7242a73980eb933b
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-18 22:53:33 +0700

    test

---
+++ athena_collective.py
@@ -0,0 +1,8 @@
+from faq import FAQ_interface
+
+
+if __name__ == "__main__":
+    company_name = "Athena Collective"
+    emoji = "🚀"
+    company_id = "athena_collective"
+    FAQ_interface(company_id, company_name, emoji)
