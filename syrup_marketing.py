0801432a5f6047c0fc8a9f9b62224d6411ad7475
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-18 22:30:08 +0700

    test

---
+++ syrup_marketing.py
@@ -0,0 +1,8 @@
+from faq import FAQ_interface
+
+
+if __name__ == "__main__":
+    company_name = "Syrup Marketing"
+    emoji = "🚀"
+    company_id = "syrup_marketing"
+    FAQ_interface(company_id, company_name, emoji)
