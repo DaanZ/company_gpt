5d018e178e05e239a4549374d64cc0019087e850
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-02-25 20:34:15 +0700

    Update syrup_marketing.py

--- syrup_marketing.py
+++ syrup_marketing.py
@@ -1,7 +1,8 @@
 from core.faq import FAQ_interface
+from core.main_interface import start_menu
 
 if __name__ == "__main__":
     company_name = "Syrup Marketing"
     emoji = "🚀"
     company_id = "syrup_marketing"
-    FAQ_interface(company_id, company_name, emoji)
+    start_menu(company_name, company_id)
