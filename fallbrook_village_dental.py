dd4be3293e95f834d3ef698cf254219f7f42ae6d
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-03-01 00:54:10 +0700

    dental

---
+++ fallbrook_village_dental.py
@@ -0,0 +1,9 @@
+
+
+from core.main_interface import start_menu
+
+if __name__ == "__main__":
+    company_name = "Fallbrook Village Dental"
+    emoji = "🦷"
+    company_id = "fallbrook_village_dental.json"
+    start_menu(company_name, company_id)
