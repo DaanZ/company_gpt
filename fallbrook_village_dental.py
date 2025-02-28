0feefe2b1a12f4f73ea1c37e72d5bbdffbab25a0
DaanZeeuwe <88703520+DaanZeeuwe@users.noreply.github.com>
2025-03-01 00:54:55 +0700

    Update fallbrook_village_dental.py

--- fallbrook_village_dental.py
+++ fallbrook_village_dental.py
@@ -5,5 +5,5 @@ from core.main_interface import start_menu
 if __name__ == "__main__":
     company_name = "Fallbrook Village Dental"
     emoji = "🦷"
-    company_id = "fallbrook_village_dental.json"
+    company_id = "fallbrook_village_dental"
     start_menu(company_name, company_id)
